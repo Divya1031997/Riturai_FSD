84f34e9f2becd417b2053a85c3778a5777c8f8af
riturai06 <59560369+riturai06@users.noreply.github.com>
2020-01-16 20:16:50 +0530

    Add files via upload

---
+++ Star.java
@@ -0,0 +1,25 @@
+/* Program to Print Star Pattern 
+Ritu Bala Rai
+*/
+
+public class Star {
+
+	
+	public static void main(String[] args) {
+			for(int j =1;j<=5;j++) {
+				for(int i =1;i<=j;i++) {
+					System.out.print("*");
+						System.out.print(" ");
+					
+					
+				}
+			
+				System.out.println();
+				
+			}
+			
+			
+			
+		}
+		                                                                                                                                 
+	}
